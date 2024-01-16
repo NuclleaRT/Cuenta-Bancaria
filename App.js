8a4f3a13254fda8a10f93dce8086295a9b7c1a38
NuclleaR <cristobalruiz1994@gmail.com>
2024-01-16 16:41:48 -0300

    added css + adrress request

--- App.js
+++ App.js
@@ -9,16 +9,19 @@ function mostrarDatos(){
     let apellido = usuario.apellido;
     let edad = usuario.edad;
     let saldo = usuario.saldo;
+    let direccion = usuario.direccion;
 
     let txtNombre = document.getElementById('nombre');
     let txtApellido = document.getElementById('apellido');
     let txtEdad = document.getElementById('edad');
     let txtSaldo = document.getElementById('saldo');
+    let txtDireccion = document.getElementById('direccion')
 
     txtNombre.textContent = "Nombre: " + nombre;
     txtApellido.textContent = "Apellido: " + apellido;
     txtEdad.textContent = "Edad: " + edad;
     txtSaldo.textContent = "Saldo: " + saldo;
+    txtDireccion.textContent = "Direccion: " + direccion.calle + " ," + direccion.ciudad;
 })
 .catch(function(error){
     alert(error);
